076166262c4504a5b057ffd875e6e793cbe9a804
Richard Lin <richard.lin@berkeley.edu>
2024-01-12 16:34:38 -0800

    server skeleton

--- server.py
+++ server.py
@@ -18,6 +18,42 @@ CACHE_FILE = "cache.ics"
 TEMPLATE_FILE = "template.svg"
 
 
+from functools import cached_property
+from http.cookies import SimpleCookie
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import parse_qsl, urlparse
+
+class WebRequestHandler(BaseHTTPRequestHandler):
+    @cached_property
+    def url(self):
+        return urlparse(self.path)
+
+    @cached_property
+    def query_data(self):
+        return dict(parse_qsl(self.url.query))
+
+    @cached_property
+    def post_data(self):
+        content_length = int(self.headers.get("Content-Length", 0))
+        return self.rfile.read(content_length)
+
+    @cached_property
+    def form_data(self):
+        return dict(parse_qsl(self.post_data.decode("utf-8")))
+
+    @cached_property
+    def cookies(self):
+        return SimpleCookie(self.headers.get("Cookie"))
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+        self.wfile.write("ducks".encode('utf-8'))
+
+    def do_POST(self):
+        self.do_GET()
+
 if __name__ == '__main__':
     template = SvgTemplate(TEMPLATE_FILE)
 
@@ -41,3 +77,7 @@ if __name__ == '__main__':
     label.append(instance)
     root = ET.ElementTree(label)
     root.write("applied.svg")
+
+    server = HTTPServer(("0.0.0.0", 8000), WebRequestHandler)
+    print("Server started")
+    server.serve_forever()
