f973a3abf7f539829186fbf2e1cee1ee40f1cf09
Richard Lin <elpato25@gmail.com>
2024-07-01 17:09:07 -0700

    multidevice mac test

---
+++ test_multidevice.py
@@ -0,0 +1,43 @@
+import unittest
+import app
+from test_common import test_get_cached_ical
+from datetime import datetime
+import pytz
+from unittest.mock import patch
+
+app.app.testing = True
+app.get_cached_ical = test_get_cached_ical
+
+
+app.kDeviceMap = {
+  'a1b1': app.DeviceRecord(
+    title="TestCalendar",
+    ical_url="TestCalendar.ics",
+    template_filename="template_3cb.svg",
+  ),
+  'a2b2': app.DeviceRecord(
+    title="EmptyCalendar",
+    ical_url="EmptyCalendar.ics",
+    template_filename="template_3cb.svg",
+  ),
+}
+
+
+class MultiDeviceTestCase(unittest.TestCase):
+  def test_a1b1(self):
+    with patch('app.datetime') as mock_datetime:
+      mock_datetime.now.return_value = datetime(2024, 7, 1, 8, 0, 0).astimezone(pytz.timezone('America/Los_Angeles'))
+
+      with app.app.test_client() as client:
+        response = client.get('/meta?mac=a1b1')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json['nextUpdateSec'], 60*60)  # event end at 9pm
+
+  def test_a2b2(self):
+    with patch('app.datetime') as mock_datetime:
+      mock_datetime.now.return_value = datetime(2024, 7, 1, 8, 0, 0).astimezone(pytz.timezone('America/Los_Angeles'))
+
+      with app.app.test_client() as client:
+        response = client.get('/meta?mac=a2b2')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json['nextUpdateSec'], 4*60*60)  # update interval at 4pm
